b8c8f68cb2769363cfc02639846d440de2460724
Dharmi26 <dharmipatel567@gmail.com>
2023-08-02 23:43:41 +0530

    authcontroller - login

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,15 @@
-const { registerController } = require("../controllers/authController");
+const { registerController, loginController } = require("../controllers/authController");
 
 const express = require('express')
 
 const router = express.Router()
 
 //routes
+
+//REGISTER
 router.post('/register',registerController)
+
+//LOGIN
+router.post('/login',loginController);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require('express')
 const dotenv = require('dotenv')
-const colors = require('colors')
 const morgan = require('morgan')
 const cors = require('cors')
 const connectDB = require('./config/db')
 
 //dot config
-dotenv.config()
+dotenv.config({path: "./vars/.env"})
 
 //Mongodb connection
 connectDB();
@@ -20,8 +19,8 @@ app.use(cors())
 app.use(morgan('dev'))
 
 //routes
-app.use('/api/v1/test',require('./routes/testRoutes'));
-app.use('/api/v1/auth', require("./routes/authRoutes"))
+app.use('/',require('./routes/testRoutes'));
+app.use('/auth', require("./routes/authRoutes"))
 
 //port
 const PORT = process.env.PORT || 8000;
